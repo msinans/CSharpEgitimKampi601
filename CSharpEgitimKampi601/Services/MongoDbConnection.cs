59bda601dabc6718cf0c46ea73a656a548fc85b8
m.sinans <msomercikoglu@gmail.com>
2024-12-25 12:51:57 +0300

    Add project files.

---
+++ CSharpEgitimKampi601/Services/MongoDbConnection.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi601.Services
+{
+    public class MongoDbConnection
+    {
+        private IMongoDatabase _database;
+
+        public MongoDbConnection()
+        {
+            var client = new MongoClient("");
+        }
+    }
+}
